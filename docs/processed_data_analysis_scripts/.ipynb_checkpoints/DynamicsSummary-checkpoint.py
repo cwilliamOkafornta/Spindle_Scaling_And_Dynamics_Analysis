514399fc7aaeb935c31b34a132a40a943e923097
Chukwuebuka William Okafornta <williamonia@gmail.com>
2024-02-16 18:28:56 +0100

    updated documentation

--- docs/processed_data_analysis_scripts/.ipynb_checkpoints/DynamicsSummary-checkpoint.py
+++ docs/processed_data_analysis_scripts/.ipynb_checkpoints/DynamicsSummary-checkpoint.py
@@ -15,7 +15,7 @@ import pandas as pd
 import numpy as np
 ####################################################################################################################################################
 
-def cellVolumeLengthValues(folder_input, folder_output, file_input, savefilename):
+def cellDynamicsValues(folder_input, folder_output, file_input, savefilename):
     # read out the cvs files
     file_list = []
     for file in file_input:
@@ -27,14 +27,21 @@ def cellVolumeLengthValues(folder_input, folder_output, file_input, savefilename
         file_list.append(df)
 
     # assign new variable to each dataFrame
-    cell_vol = file_list[0]
-    chromosomes_dist = file_list[1]
-    poles_dist = file_list[2]
-    pole_chromosomes = file_list[3]
+    for i in file_list:
+        try:
+            cell_vol = file_list[0]  # cell volume 
+            chromosomes_dist = file_list[1]  # chromosome-to-chromosome distance
+            poles_dist = file_list[2]  # pole-to-pole distance
+            pole_chromosomes = file_list[3] # pole-to-chromosome distance 
+            metaphase_poles = file_list[4]  # metaphase pole-to-pole distance
+            elongation_poles = file_list[5] # elongation rate pole-to-pole distance
+            segregation_chromosomes = file_list[6] # segregation rate for chromosome-to-chromosome distance
+        except Exception:
+            pass
 
     # calculate for cell length by calculating the cubic square of the cell volume
     cell_length = cell_vol['Vol (µm³)']**(1/3)
-    cell_vol['Cell length [µm]'] = cell_length
+    cell_vol['Cell length (µm)'] = cell_length
 
     # calculate for the propagation error of uncertainty of the cell length
     '''
@@ -44,6 +51,7 @@ def cellVolumeLengthValues(folder_input, folder_output, file_input, savefilename
     Error_prop = abs((1/3) * cell_vol['Vol (µm³)']**(-2/3) * cell_vol['std_Vol'])
     cell_vol['Error propagation'] = Error_prop
 
+   
     # chromosome-to-chromosome final distance
     dist_mean_chromosomes = pd.DataFrame()
     mean_chromosomes_std = pd.DataFrame()
@@ -80,6 +88,50 @@ def cellVolumeLengthValues(folder_input, folder_output, file_input, savefilename
         new_val_PC_std = pd.DataFrame.from_dict({f"{col_PC}": np.std(pole_chromosomes[col_PC])}, orient='index', columns=['P-to-C_std'])
         mean_PC_std = pd.concat([mean_PC_std, new_val_PC_std], axis=0)
 
-    newTable = pd.concat([dist_mean_chromosomes, mean_chromosomes_std, dist_mean_poles, mean_poles_std, dist_mean_PC, mean_PC_std], axis=1).reset_index(drop=True)
-    newTable = pd.concat([cell_vol, newTable], axis=1)
-    newTable.to_csv(os.path.join(folder_output, savefilename+'.csv'), index=False) # save csv
+    # metaphase pole-to-pole distance 
+    dist_mean_meta_pp = pd.DataFrame()
+    mean_meta_pp_std = pd.DataFrame()
+    for col_meta in metaphase_poles:
+        '''mean metaphase pole-to-pole distance'''
+        new_val_meta_temp = pd.DataFrame.from_dict({f"{col_meta}": np.mean(metaphase_poles[col_meta])}, orient='index', columns=['Metaphase_P-to-P (µm)'])
+        dist_mean_meta_pp = pd.concat([dist_mean_meta_pp, new_val_meta_temp], axis=0)
+
+        '''std metaphase pole-to-pole distance'''
+        new_val_meta_std = pd.DataFrame.from_dict({f"{col_meta}": np.std(metaphase_poles[col_meta])}, orient='index', columns=['Metaphase_P-to-P_std'])
+        mean_meta_pp_std = pd.concat([mean_meta_pp_std, new_val_meta_std], axis=0)
+        
+    # elongation rate for pole-to-pole distance
+    dist_mean_elong = pd.DataFrame()
+    mean_elong_std = pd.DataFrame()
+    for col_elong in elongation_poles:
+        '''mean elongation pole-to-pole distance'''
+        new_val_elong_temp = pd.DataFrame.from_dict({f"{col_elong}": np.mean(elongation_poles[col_elong])}, orient='index', columns=['elongation_P-to-P (µm/min)'])
+        dist_mean_elong = pd.concat([dist_mean_elong, new_val_elong_temp], axis=0)
+
+        '''std elongation pole-to-pole distance'''
+        new_val_elong_std = pd.DataFrame.from_dict({f"{col_elong}": np.std(elongation_poles[col_elong])}, orient='index', columns=['elongation_P-to-P_std'])
+        mean_elong_std = pd.concat([mean_elong_std, new_val_elong_std], axis=0)
+        
+     # segregation chromosome-to-chromosome distance 
+    dist_mean_segreg = pd.DataFrame()
+    mean_segreg_std = pd.DataFrame()
+    for col_segreg in segregation_chromosomes:
+        '''mean segregation chromosome-to-chromosome distance'''
+        new_val_segreg_temp = pd.DataFrame.from_dict({f"{col_segreg}": np.mean(segregation_chromosomes[col_segreg])}, orient='index', columns=['segregation_C-to-C (µm/min)'])
+        dist_mean_segreg = pd.concat([dist_mean_segreg, new_val_segreg_temp], axis=0)
+
+        '''std segregation chromosome-to-chromosome distance'''
+        new_val_segreg_std = pd.DataFrame.from_dict({f"{col_segreg}": np.std(segregation_chromosomes[col_segreg])}, orient='index', columns=['segregation_C-to-C_std'])
+        mean_segreg_std = pd.concat([mean_segreg_std, new_val_segreg_std], axis=0)
+        
+    
+    newTable = pd.concat([dist_mean_chromosomes, mean_chromosomes_std,
+                          dist_mean_poles, mean_poles_std, 
+                          dist_mean_PC, mean_PC_std,
+                          dist_mean_meta_pp, mean_meta_pp_std,
+                          dist_mean_elong, mean_elong_std,
+                          dist_mean_segreg, mean_segreg_std],
+                         axis=1).reset_index(drop=True)
+    
+    new_df = pd.concat([cell_vol, newTable], axis=1)
+    new_df.to_csv(os.path.join(folder_output, savefilename+'.csv'), index=False, encoding='cp1252') # save csv
---
+++ docs/processed_data_analysis_scripts/DynamicsAnalysisSummary.py
@@ -0,0 +1,219 @@
+# library packages
+import os
+import pandas as pd
+import numpy as np
+
+# load files
+def fileLoad(folder_input, file_input):
+    file_list = []
+    for file in file_input:
+        file_path = os.path.join(folder_input, file)
+        try:
+            df = pd.read_csv(file_path, index_col=None, encoding='utf-8')
+        except:
+            df =pd.read_csv(file_path, index_col=None, encoding='cp1252')
+        file_list.append(df)
+    return file_list 
+
+# calculate for cell length from the cell volume
+def cellVolumeLength(cell_volume_file_number, parameter_files):
+    cell_volume = parameter_files[cell_volume_file_number]
+    cell_vol = cell_volume['Vol (µm³)']
+    cell_volume_std = cell_volume['std_Vol']
+    
+    # calculate for cell length
+    cell_length = cell_vol**(1/3)
+    cell_volume['Cell length (µm)'] = cell_length
+    
+    # calculate for cell length propagation error
+    Error_prop = abs((1/3) * cell_vol**(-2/3) * cell_volume_std)
+    cell_volume['Error propagation'] = Error_prop
+    
+    return cell_volume
+
+# calculate for the mean and standard deviation of the chromosome-to-chromosome distance for each embryonic cell stage
+def chromosomeDistance(chromosome_file_number, parameter_files):
+    chromosome_to_chromosome_distance = parameter_files[chromosome_file_number]
+    chromosome_mean = pd.DataFrame()
+    chromosome_std = pd.DataFrame()
+    for col_chromosome in chromosome_to_chromosome_distance:
+        '''calculate for mean chromosome-to-chromosome distance for each embryonic cell stage'''
+        chrom_temp_val_mean = pd.DataFrame.from_dict({f"{col_chromosome}": np.mean(chromosome_to_chromosome_distance[col_chromosome])},
+                                                     orient='index',
+                                                     columns=['Final chromosome-to-chromosome (µm)'])
+        
+        '''calculate for standard deviation chromosome-to-chromosome distance for each embryonic cell stage'''
+        chrom_temp_val_std = pd.DataFrame.from_dict({f"{col_chromosome}": np.std(chromosome_to_chromosome_distance[col_chromosome])},
+                                                     orient='index',
+                                                     columns=['Final chromosome-to-chromosome std'])
+        
+        # new chromosome-to-chromosome mean and standard deviation tables
+        chromosome_mean = pd.concat([chromosome_mean, chrom_temp_val_mean], axis=0)
+        chromosome_std = pd.concat([chromosome_std, chrom_temp_val_std], axis=0)
+        
+    # new table
+    chromosome_distance = pd.concat([chromosome_mean, chromosome_std], axis=1)
+    return chromosome_distance
+
+# calculate for the mean and standard deviation of the pole-to-pole distance for each embryonic cell stage
+def poleDistance(poles_file_number, parameter_files):
+    pole_to_pole_distance = parameter_files[poles_file_number]
+    poles_mean = pd.DataFrame()
+    poles_std = pd.DataFrame()
+    for col_pole in pole_to_pole_distance:
+        '''calculate for mean pole-to-pole distance for each embryonic cell stage'''
+        pole_temp_val_mean = pd.DataFrame.from_dict({f"{col_pole}": np.mean(pole_to_pole_distance[col_pole])}, orient='index', columns=['Final pole-to-pole (µm)'])
+        
+        '''calculate for standard deviation of the pole-to-pole distance for each embryonic cell stage'''
+        pole_temp_val_std = pd.DataFrame.from_dict({f"{col_pole}": np.std(pole_to_pole_distance[col_pole])}, orient='index', columns=['Final pole-to-pole std'])
+        
+        # new pole-to-pole mean and standard deviation tables
+        poles_mean = pd.concat([poles_mean, pole_temp_val_mean], axis=0)
+        poles_std = pd.concat([poles_std, pole_temp_val_std], axis=0)
+        
+    # new table 
+    pole_distance = pd.concat([poles_mean, poles_std], axis=1)
+    return pole_distance
+
+# calculate for the mean and standard deviation of the pole-to-chromosome distance for each embryonic cell stage
+def poleChromosomeDistance(pole_chromosome_file_number, parameter_files):
+    pole_to_chromosome_distance = parameter_files[pole_chromosome_file_number]
+    pole_chrom_mean = pd.DataFrame()
+    pole_chrom_std = pd.DataFrame()
+    for col_pole_chrom in pole_to_chromosome_distance:
+        '''calculate for mean pole-to-chromosome distance for each embryonic cell stage'''
+        pole_chrom_temp_mean = pd.DataFrame.from_dict({f"{col_pole_chrom}": np.mean(pole_to_chromosome_distance[col_pole_chrom])},
+                                                      orient='index', columns=['Final pole-to-chromosome (µm)'])
+        
+        '''calculate for standard pole-to-chromosome distance for each embryonic cell stage'''
+        pole_chrom_temp_std = pd.DataFrame.from_dict({f"{col_pole_chrom}": np.std(pole_to_chromosome_distance[col_pole_chrom])},
+                                                      orient='index', columns=['Final pole-to-chromosome std'])
+        
+        # new pole-to-chromosome mean and standard deviation tables
+        pole_chrom_mean = pd.concat([pole_chrom_mean, pole_chrom_temp_mean], axis=0)
+        pole_chrom_std = pd.concat([pole_chrom_std, pole_chrom_temp_std], axis=0)
+        
+    # new tables
+    polesChroms_distance = pd.concat([pole_chrom_mean, pole_chrom_std], axis=1)
+    return polesChroms_distance
+
+# calculate for the mean and standard deviation of the metaphase pole-to-pole distance for each embryonic cell stage
+def metaphasePoleDistance(metaphase_pole_file_number, parameter_files):
+    metaphase_pole_to_pole_distance = parameter_files[metaphase_pole_file_number]
+    metaphase_pole_mean = pd.DataFrame()
+    metaphase_pole_std = pd.DataFrame()
+    for col_metaphase in metaphase_pole_to_pole_distance:
+        '''calculate for mean metaphase pole-to-pole distance for each embryonic cell stage'''
+        meta_pole_temp_mean = pd.DataFrame.from_dict({f"{col_metaphase}": np.mean(metaphase_pole_to_pole_distance[col_metaphase])},
+                                                      orient='index', columns=['Metaphase pole-to-pole (µm)'])
+        
+        '''calculate for standard deviation metaphase pole-to-pole distance for each embryonic cell stage'''
+        meta_pole_temp_std = pd.DataFrame.from_dict({f"{col_metaphase}": np.std(metaphase_pole_to_pole_distance[col_metaphase])},
+                                                      orient='index', columns=['Metaphase pole-to-pole std'])
+        
+        # new metaphase pole-to-pole mean and standard deviation tables
+        metaphase_pole_mean = pd.concat([metaphase_pole_mean, meta_pole_temp_mean], axis=0)
+        metaphase_pole_std = pd.concat([metaphase_pole_std, meta_pole_temp_std], axis=0)
+        
+    # new tables
+    metaphase_distance = pd.concat([metaphase_pole_mean, metaphase_pole_std], axis=1)
+    return metaphase_distance
+
+# calculate for the mean and standard deviation of the pole-to-pole elongation rate for each embryonic cell stage
+def elongationRatePoles(elongation_speed_file_number, parameter_files):
+    elongation_speed_pole_to_pole = parameter_files[elongation_speed_file_number]
+    elongation_speed_mean = pd.DataFrame()
+    elongation_speed_std = pd.DataFrame()
+    for col_elongation in elongation_speed_pole_to_pole:
+        '''calculate for mean pole-to-pole distance elongation rate for each embryonic cell stage'''
+        elong_speed_temp_mean = pd.DataFrame.from_dict({f"{col_elongation}": np.mean(elongation_speed_pole_to_pole[col_elongation])},
+                                                      orient='index', columns=['Elongation rate pole-to-pole (µm/min)'])
+        
+        '''calculate for standard deviation pole-to-pole distance elongation rate for each embryonic cell stage'''
+        elong_speed_temp_std = pd.DataFrame.from_dict({f"{col_elongation}": np.std(elongation_speed_pole_to_pole[col_elongation])},
+                                                      orient='index', columns=['Elongation rate pole-to-pole std'])
+        
+        # new pole-to-pole elongation rate mean and standard deviation tables
+        elongation_speed_mean = pd.concat([elongation_speed_mean, elong_speed_temp_mean], axis=0)
+        elongation_speed_std = pd.concat([elongation_speed_std, elong_speed_temp_std], axis=0)
+        
+    # new tables
+    elongation_speed = pd.concat([elongation_speed_mean, elongation_speed_std], axis=1)
+    return elongation_speed
+
+# calculate for the mean and standard deviation of the chromosome-to-chromosome segregation speed for each embryonic cell stage
+def segregationSpeedChromosomes(segregation_speed_file_number, parameter_files):
+    segregation_speed_chromosome_to_chromosome = parameter_files[segregation_speed_file_number]
+    segregation_speed_mean = pd.DataFrame()
+    segregation_speed_std = pd.DataFrame()
+    for col_segregation in segregation_speed_chromosome_to_chromosome:
+        '''calculate for mean chromosome-to-chromosome distance segregation speed for each embryonic cell stage'''
+        elong_speed_temp_mean = pd.DataFrame.from_dict({f"{col_segregation}": np.mean(segregation_speed_chromosome_to_chromosome[col_segregation])},
+                                                      orient='index', columns=['segregation speed chromosome-to-chromosome (µm/min)'])
+        
+        '''calculate for standard deviation chromosome-to-chromosome distance segregation speed for each embryonic cell stage'''
+        elong_speed_temp_std = pd.DataFrame.from_dict({f"{col_segregation}": np.std(segregation_speed_chromosome_to_chromosome[col_segregation])},
+                                                      orient='index', columns=['segregation speed chromosome-to-chromosome std'])
+        
+        # new chromosome-to-chromosome segregation speed mean and standard deviation tables
+        segregation_speed_mean = pd.concat([segregation_speed_mean, elong_speed_temp_mean], axis=0)
+        segregation_speed_std = pd.concat([segregation_speed_std, elong_speed_temp_std], axis=0)
+        
+    # new tables
+    segregation_speed = pd.concat([segregation_speed_mean, segregation_speed_std], axis=1)
+    return segregation_speed
+
+# calculate for the mean and standard deviation of the initial pole-to-pole distance for each embryonic cell stage
+def initialPoleDistance(initial_pole_file_number, parameter_files):
+    initial_pole_distance = parameter_files[initial_pole_file_number]
+    initial_mean = pd.DataFrame()
+    initial_std = pd.DataFrame()
+    for col_initial in initial_pole_distance:
+        '''calculate for mean initial pole-to-pole distance for each embryonic cell stage'''
+        initial_temp_val_mean = pd.DataFrame.from_dict({f"{col_initial}": np.mean(initial_pole_distance[col_initial])}, orient='index', columns=['Initial pole-to-pole (µm)'])
+        
+        '''calculate for std initial pole-to-pole distance for each embryonic cell stage'''
+        initial_temp_val_std = pd.DataFrame.from_dict({f"{col_initial}": np.std(initial_pole_distance[col_initial])}, orient='index', columns=['Initial pole-to-pole std'])
+        
+        # new initial pole-to-pole distance mean and standard deviation tables
+        initial_mean = pd.concat([initial_mean, initial_temp_val_mean], axis=0)
+        initial_std = pd.concat([initial_std, initial_temp_val_std], axis=0)
+        
+    # new tables
+    initial_poles = pd.concat([initial_mean, initial_std], axis=1)
+    return initial_poles
+
+# make a table with spindle dynamics values and save the table as a csv file in the save folder
+def spindleDynamicsValues(cell_volume_file_number,
+                          chromosome_file_number,
+                          poles_file_number,
+                          pole_chromosome_file_number,
+                          metaphase_pole_file_number,
+                          elongation_speed_file_number,
+                          segregation_speed_file_number,
+                          initial_pole_file_number,
+                          folder_input,
+                          file_input,
+                          save_folder,
+                          save_file_name,
+                          embryo_condition):
+    
+    parameter_files = fileLoad(folder_input, file_input)
+    
+    cell_volume_length = cellVolumeLength(cell_volume_file_number, parameter_files)
+    final_chromosome_length = chromosomeDistance(chromosome_file_number, parameter_files)
+    final_poles_length = poleDistance(poles_file_number, parameter_files)
+    final_pole_chromosome = poleChromosomeDistance(pole_chromosome_file_number, parameter_files)
+    metaphase_length = metaphasePoleDistance(metaphase_pole_file_number, parameter_files)
+    elongation = elongationRatePoles(elongation_speed_file_number, parameter_files)
+    segregation = segregationSpeedChromosomes(segregation_speed_file_number, parameter_files)
+    initial_pole_length = initialPoleDistance(initial_pole_file_number, parameter_files)
+    
+    # table with spindle dynamics values
+    newTable = pd.concat([initial_pole_length, final_poles_length, final_chromosome_length, final_pole_chromosome, metaphase_length, elongation, segregation], axis=1).reset_index(drop=True)
+    new_df = pd.concat([cell_volume_length, newTable], axis=1)
+    
+    # save table as csv file
+    csv_new_df = new_df.to_csv(os.path.join(save_folder, save_file_name + '(' + embryo_condition + ')' + '.csv'), index=False, encoding='cp1252')
+    
+    return csv_new_df
